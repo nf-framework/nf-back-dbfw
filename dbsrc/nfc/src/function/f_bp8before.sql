2795e41e54bd50102ea64e857198274ceed76ab3
gnaumova <billanda2@gmail.com>
2022-06-01 14:50:33 +0300

    check privs on broker

--- dbsrc/nfc/src/function/f_bp8before.sql
+++ dbsrc/nfc/src/function/f_bp8before.sql
@@ -1,11 +1,13 @@
-CREATE OR REPLACE FUNCTION nfc.f_bp8before(p_unitbp character varying, p_unit_id text, p_org bigint, p_grp bigint DEFAULT NULL::bigint, p_cid bigint DEFAULT NULL::bigint)
+CREATE OR REPLACE FUNCTION nfc.f_bp8before(p_unitbp character varying, p_unit_id text, p_org bigint, p_grp bigint DEFAULT NULL::bigint, p_cid bigint DEFAULT NULL::bigint, p_use_privs boolean DEFAULT true)
  RETURNS void
  LANGUAGE plpgsql
  SECURITY DEFINER
 AS $function$
 begin
   -- проверка прав
+  if p_use_privs then
 	perform nfc.f4role_unitbpprivs8chk(p_unitbp, p_org);
+  end if;
   -- выполнение событийных процедур ДО действия
   --perform nfc.f4eventprocs8auto_execute(p_unitbp,p_unit_id,0,p_org,p_grp,p_cid);
   -- выполнение пользовательских процедур ДО действия
@@ -15,4 +17,3 @@ begin
 end;
 $function$
 ;
-comment on function nfc.f_bp8before(p_unitbp character varying, p_unit_id text, p_org bigint, p_grp bigint, p_cid bigint) is 'Пролог бизнес-действия';
