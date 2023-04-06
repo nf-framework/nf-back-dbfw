06617a132c969fc1aceac658df58c3f00dada6f2
Андрей Максимов <andrmaxmail@gmail.com>
2023-04-07 00:18:46 +0300

    added get and find new actions to broker

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 export * as models from './models/index.js';
 import { Env } from './lib/env.js';
 import { Op, Model } from './lib/model.js';
+import { getUnit, getUnitBp } from './lib/broker.js';
 export * as testHelper from './lib/test-helper.js';
 export * as modelGen from './lib/modelgen.js';
 
 export {
     Model,
     Env,
-    Op
+    Op,
+    getUnit,
+    getUnitBp
 };
